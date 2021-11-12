dbfdda020fad05f3c523e4be501487fd2023f3c4
teha008 <82374423+teha008@users.noreply.github.com>
2021-11-13 00:53:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { Application, Router } from 'https://deno.land/x/oak/mod.ts'
-import { oakCors } from 'https://deno.land/x/cors/mod.ts'
+import { Application, Router } from 'https://deno.land/x/oak@v7.7.0/mod.ts'
+import { oakCors } from 'https://deno.land/x/cors@v1.2.2/mod.ts'
 
 const messages = []
 const channel = new BroadcastChannel('chat')
