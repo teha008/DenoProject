f1267bd3f0ed2271e8e844c95c330ef4af25e510
chosangho <teha007@naver.com>
2021-11-12 11:15:07 +0900

    change

--- deno-chat-pai/index.js
+++ deno-chat-pai/index.js
@@ -1,6 +1,9 @@
-addEventListener('fetch', (event) => {
-  const response = new Response('hello world', {
-    headers: { 'Content-Type': 'text/plain' },
-  })
-  event.respondWith(response)
+import { Application } from 'https://deno.land/x/oak/mod.ts'
+
+const app = new Application()
+
+app.use((ctx) => {
+  ctx.response.body = 'Hello World Third try!'
 })
+
+addEventListener('fetch', app.fetchEventHandler())
