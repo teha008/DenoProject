42aae470b645317b6f6290607f46238a6d59ebaa
chosangho <teha007@naver.com>
2021-11-12 15:10:27 +0900

    update

---
+++ deno-chat-client/pages/index.tsx
@@ -0,0 +1,41 @@
+/** @jsx h */
+import { h, IS_BROWSER, useState, useEffect, useCallback } from '../deps.ts'
+
+interface Message {
+  text: string
+}
+
+export default function Home() {
+  const [messages, setMessages] = useState<Message[]>([])
+  const [text, setText] = useState('')
+
+  const getMessages = useCallback(async () => {
+    const res = await fetch('https://cho-deno-chat-api.deno.dev/messages')
+    const data = await res.json()
+    setMessages(data)
+  }, [])
+
+  useEffect(() => {
+    getMessages()
+  }, [])
+
+  const onSendMessage = useCallback(async () => {
+    await fetch('https://cho-deno-chat-api.deno.dev/messages', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        text,
+      }),
+    })
+  }, [text])
+
+  return (
+    <div>
+      <div>{JSON.stringify(messages)}</div>
+      <input type="text" value={text} onChange={(evt) => setText(evt.target.value)} />
+      <button onClick={onSendMessage}>Add</button>
+    </div>
+  )
+}
